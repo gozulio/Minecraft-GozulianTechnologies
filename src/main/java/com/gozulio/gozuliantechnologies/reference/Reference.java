082bb180da386138141b20076171ce598f69ac5b
Michael Gosselin <gozulio@gmail.com>
2015-02-26 05:45:47 -0500

    Set up main class
    Set up main mod class and imported a log helperutility.

---
+++ src/main/java/com/gozulio/gozuliantechnologies/reference/Reference.java
@@ -0,0 +1,10 @@
+package com.gozulio.gozuliantechnologies.reference;
+
+public class Reference
+{
+    //Mod Information.
+    public static final String MOD_ID = "gozuliantechnologies";
+    public static final String MOD_NAME = "Gozulian Technologies";
+    public static final String VERSION = "1.7.10-0.100";
+
+}
