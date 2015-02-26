3cfd01f6515d8fa885b08bf7c87686da70026311
Michael Gosselin <gozulio@gmail.com>
2015-02-26 18:49:43 -0500

    0.100
    Corrected Version. Committing a basic sword class setup. will have to
    rewrite part of it at some point

---
+++ src/main/java/com/gozulio/gozuliantechnologies/helper/tool/_ToolHelper_Generic.java
@@ -0,0 +1,12 @@
+package com.gozulio.gozuliantechnologies.helper.tool;
+
+import com.gozulio.gozuliantechnologies.helper._ItemHelper_Generic;
+
+public class _ToolHelper_Generic extends _ItemHelper_Generic
+{
+    public _ToolHelper_Generic()
+    {
+        super();
+        this.setMaxStackSize(1);
+    }
+}
--- src/main/java/com/gozulio/gozuliantechnologies/init/modItems.java
+++ src/main/java/com/gozulio/gozuliantechnologies/init/modItems.java
@@ -1,24 +1,33 @@
 package com.gozulio.gozuliantechnologies.init;
 
 import com.gozulio.gozuliantechnologies.helper._ItemHelper_Generic;
+import com.gozulio.gozuliantechnologies.helper.tool._ToolHelper_Generic;
 import com.gozulio.gozuliantechnologies.item.itemTiberiumBlue;
 import com.gozulio.gozuliantechnologies.item.itemTiberiumGreen;
 import com.gozulio.gozuliantechnologies.item.itemTiberiumRed;
+import com.gozulio.gozuliantechnologies.item.tool.sword.toolTiberiumGreenSword;
 import com.gozulio.gozuliantechnologies.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 @GameRegistry.ObjectHolder((Reference.MOD_ID))
 public class modItems
 {
+    //declare items
     public static final _ItemHelper_Generic itemtiberiumgreen = new itemTiberiumGreen();
     public static final _ItemHelper_Generic itemtiberiumblue = new itemTiberiumBlue();
     public static final _ItemHelper_Generic itemtiberiumred = new itemTiberiumRed();
 
+    //declare swords
+    public static final _ToolHelper_Generic tooltiberiumgreensword = new toolTiberiumGreenSword();
 
     public static void init()
     {
+        //init base items
         GameRegistry.registerItem(itemtiberiumgreen, "Tiberium_Green");
         GameRegistry.registerItem(itemtiberiumblue, "Tiberium_Blue");
         GameRegistry.registerItem(itemtiberiumred, "Tiberium_Red");
+
+        //init swords
+        GameRegistry.registerItem(tooltiberiumgreensword , "Tiberium_Green_Sword");
     }
 }
